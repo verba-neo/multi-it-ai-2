0091c781ef9bd7036ec1770cd857890669dd4550
looloo404 <looloo404@hanmail.net>
2023-02-10 10:41:34 +0900

    천번째 푼거

--- 강지윤/main.py
+++ 강지윤/main.py
@@ -3,14 +3,16 @@
 # Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
 # 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 Shift 두 번을(를) 누릅니다.
 
+def solution(n):
+    sum = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            sum += i
 
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
-
+    return sum
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    solution(100)
 
 
