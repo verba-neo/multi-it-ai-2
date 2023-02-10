aa98d68abaeaaf31b40a72bafba8515a1c7129e1
minu-97 <alsdn9891@hanyang.ac.kr>
2023-02-10 17:31:22 +0900

    s1859

---
+++ s1859-백만장자프로젝트/조민우.py
@@ -0,0 +1,20 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+T= int(input())
+# for tc in range(1, T+1):
+#     N = int(input())
+#     numbers = list(map(int, input().split()))
+#     sell_price = 0
+#     last_day = numbers[-1]
+#     for i in range(len(numbers)-1, -1, -1):
+#         if last_day > numbers[i]:
+#             sell_price += last_day - numbers[i]
+#         else:
+#             last_day = numbers[i]
+
+    for i in range(len(numbers)-1):
+        if numbers[-1] > numbers[i]:
+            self_price = last_day - numbers[i]
+
+    print(f'#{tc} {sell_price}')
