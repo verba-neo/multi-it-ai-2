626a67f4fd1a7d73f7319c9e3be9f8625c4aaf6f
edu-neo <verba.it.corp@gmail.com>
2023-02-10 17:53:44 +0900

    add comment

--- s1859-백만장자프로젝트/neo.py
+++ s1859-백만장자프로젝트/neo.py
@@ -14,12 +14,13 @@ for tc in range(1, T+1):
         # 지금 보고 있는 가격
         price = prices[idx]
         
-        # 지금 가격이 미래 가격보다 싸니까 무조건 삼 
-        # 미래 최대가격에서 현재 구매가격 뺀 값이 수익
         if price > max_price:
             # 팔아야 하는 가격
             max_price = price
+        
+        # 지금 가격이 팔아야 하는 가격보다 쌈
         else:
+            # 고로 바로 이윤 계산 가능
             profit = max_price - price
             total += profit
 
