e6653d47055a8297c22a44bf9eb4f6f48d5b9d85
minu-97 <alsdn9891@hanyang.ac.kr>
2023-02-13 16:59:30 +0900

    solved

--- s2001-파리퇴치/조민우.py
+++ s2001-파리퇴치/조민우.py
@@ -1,9 +1,20 @@
 import sys
-sys.stdin = open("input.txt", "r")
-T = int(input())
 
-for test_case in range(1, T + 1):
-   N, K = map(int, input().split())
-   # N * N 행렬
-   matrix = [list(map(int, input().split())) for i in range(N)]
-   part_mat = git
+sys.stdin = open('input.txt')
+
+T = int(input()) #테스트케이스 개수
+
+for tc in range(1, T + 1):
+    N, K = map(int,input().split())
+    matrix = [list(map(int, input().split())) for _ in range(N)]
+    SumList = []
+    for i in range(N-K-1):
+        for j in range(N-K-1):
+
+            sum = 0
+            for F_C in range(K):
+                for F_R in range(K):
+                    sum += matrix[i+F_C][j+F_R]
+                SumList.append(sum)
+
+    print(f'#{tc} {max(SumList)}')
