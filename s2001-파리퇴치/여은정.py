ba9b430f8b9ee107f59d50ed4dc658fcac52ff91
dsforpsychology <dsforpsychology@naver.com>
2023-02-13 13:17:26 +0900

    코드 정리

--- s2001-파리퇴치/여은정.py
+++ s2001-파리퇴치/여은정.py
@@ -1,36 +1,33 @@
 import sys
-sys.stdin = open('./input.txt')  #  test case 읽어 오기 
 
-def flyCatch():
-    T = int(input()) # 숫자로 데이터 인풋 
-    for tc in range(1, T+1):
-        
-        N,M = map(int,input().split())
-        #print(f'N:{N}')
-        #print(f'M:{M}')
+sys.stdin = open('./input.txt')  # 읽어 오기
+
+
+def flycatch():
+    t = int(input())  # 숫자로 데이터 인풋
+    for tc in range(1, t + 1):
+
+        n, m = map(int, input().split())
+
         # 2차원 배열 만들기 
-        arr = [[0 for j in range(N)] for i in range(N)]
-        
-        
+        arr = [[0 for j in range(n)] for i in range(n)]
+
         # 배열 정렬하기 
-        for i in range(0, N):
-            #temparr = list(map(int, input().split()))
+        for i in range(0, n):
+            # temparr = list(map(int, input().split()))
             arr[i] = list(map(int, input().split()))
-            # 
-        # print(arr)
-        
+            
         sumValue = 0
-        # 파리 잡고 
-        for j in range(M-1, 2*M-1):
-            if (j < N) :
-                for k in range(M-1, 2*M-1):
+        # m의 배열의 수 만큼 파리 잡고 
+        for j in range(m - 1, 2 * m - 1):
+            if (j < n):
+                for k in range(m - 1, 2 * m - 1):
                     # print(f'[{j}][{k}]')
-                    if (k < N ) :
+                    if (k < n):
                         # print(arr[j][k])
-                        sumValue+= arr[j][k]
-                
+                        sumValue += arr[j][k]
+
         print(f'#{tc} {sumValue}')
 
-flyCatch() 
-    
-	
+
+flycatch()
