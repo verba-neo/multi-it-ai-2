f9cc5b3cac3b34e2b70b0194555b43fb14870a80
minu-97 <alsdn9891@hanyang.ac.kr>
2023-02-13 13:23:33 +0900

    s1859

--- s1859-백만장자프로젝트/조민우.py
+++ s1859-백만장자프로젝트/조민우.py
@@ -4,12 +4,15 @@ sys.stdin = open("input.txt", "r")
 T= int(input())
 for tc in range(1, T+1):
     N = int(input())
-    numbers = list(map(int, input().split()))
+    prices = list(map(int, input().split()))
     sell_price = 0
-    last_day = numbers[-1]
-    for i in range(len(numbers)-1, -1, -1):
-        if last_day > numbers[i]:
-            sell_price += last_day - numbers[i]
+    total = 0
+    last_day = prices[-1]
+    for i in range(len(prices)-1, -1, -1):
+        if prices[i] > last_day:
+            last_day = prices[i]
         else:
-            last_day = numbers[i]
-    print(f'#{tc} {sell_price}')
+            sell_price = last_day - prices[i]
+            total += sell_price
+
+    print(f'#{tc} {total}')
