4421c81409cc87e48544f5ef682a06fe3a078073
fdslfoiedsf <khow901@nate.com>
2023-02-13 09:16:24 +0900

    Merge branch 'master' of https://github.com/edu-neo/multi-it-ai-2

--- s1859-백만장자프로젝트/김현우.py
+++ s1859-백만장자프로젝트/김현우.py
@@ -12,3 +12,17 @@ for i in range(1, t + 1):
     prices = list(map(int, input().strip().split()))
     result = max_profit(n, prices)
     print(f"#{i} {result}")
+
+
+    for to in rang(1,10/)
+        n - int (impjt))
+        preic es- lsit(ma(int, input()psid;fd
+        tortla=-))
+        max _ price = 0
+
+        for idx in range(/n01.01.10
+        rie _rpci di
+        if price> max _price:
+            prifit- max _orpic- pricemw
+            )
+
