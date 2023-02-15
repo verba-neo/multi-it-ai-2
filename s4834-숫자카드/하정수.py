07412c4bc1052a6fc1f6c39912deed90dce08795
Eric4848 <hajungsu88@gmail.com>
2023-02-15 17:28:49 +0900

    comment

--- s4834-숫자카드/하정수.py
+++ s4834-숫자카드/하정수.py
@@ -3,13 +3,13 @@ sys.stdin = open("input.txt", "r")
 
 T = int(input())
 for test_case in range(1, T + 1):
-    length = int(input())
-    numbers = [int(n[0:1]) for n in input()]
-    maximum = 0
-    max_num = 0
+    length = int(input())   # 입력 개수를 저장한다.
+    numbers = [int(n[0:1]) for n in input()]   # 입력값들을 저장한다.
+    maximum = 0   # 가장 많이 나온 횟수를 0으로 초기화한다.
+    max_num = 0   # 가장 많이 나온 숫자를 0으로 초기화한다.
 
-    for i in range(10):
-        if numbers.count(i) >= maximum:
+    for i in range(10):   # 0부터 10까지 반복
+        if numbers.count(i) >= maximum:   # i의 개수를 세어 최댓값보다 크면 그 숫자와 갯수를 저장한다.
             maximum = numbers.count(i)
             max_num = i
 
