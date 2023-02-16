dd0168b14d24677ce32a87bb22802d460e62012f
leeyonghee1017 <leeyonghe90@gmail.com>
2023-02-16 10:20:28 +0900

    s1210

--- s4836-색칠하기/이용희.py
+++ s4836-색칠하기/이용희.py
@@ -13,9 +13,9 @@ for test_case in range(1, T+1):
     result = 0
     # 색깔 좌표 갯수 만큼 반복
     for _ in range(N):
-        # 좌표(리스트)와 색깔을 저장
+        # 좌표(리스트)와 색깔을 저장 [2 2 4 4] 1
         *coordinate, color = list(map(int, input().split()))
-        # 색깔이 레드(1)일 경우
+        # 색깔이 레드(1)일 경우 [2, 2, 4, 4] 1(red)
         if color == 1:
             # 좌표 리스트에서 X의 최소값에서 최대값 + 1 까지 반복
             for x in range(coordinate[0], coordinate[2]+1):
@@ -24,7 +24,7 @@ for test_case in range(1, T+1):
                     # X, Y 값을 레드 리스트에 저장
                     if not red_list.count([x, y]):
                         red_list.append([x, y])
-        # 색깔이 블루(2)일 경우
+        # 색깔이 블루(2)일 경우 [3, 3, 6, 6] 2(blue)
         else:
             # 좌표 리스트에서 X의 최소값에서 최대값 + 1 까지 반복
             for x in range(coordinate[0], coordinate[2]+1):
