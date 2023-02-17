a9a6061fc09422d4092ba07a9d855bf12d91dbf1
fdslfoiedsf <khow901@nate.com>
2023-02-17 16:27:58 +0900

    Merge branch 'master' of https://github.com/edu-neo/multi-it-ai-2 into 김현우

---
+++ p-송아지찾기/김현우.py
@@ -0,0 +1,48 @@
+def solution(s,e):
+    # 몇 번만ㅊ에 찾앗는지
+count = 0
+# 목적지
+to _visties = [s]
+for _ in_vistie:
+    # 목적지 횟수 만큼 반복
+  for _ in range(len(to_vistis)):
+      # 현재 위치 갱신
+    currrent = to_visits .pos90)
+
+      #방문한 적 없다면 ,
+      if not visited[cuttetn]:True
+      if current == 0:
+          return coutnt
+     이동할 수 잇는 선ㅌ개지들
+      for nxt in [current-1, cuttent +1 , current +5 ]:
+          if 0  < nxt M = 1000000 and not vistied[nxt]:
+              to_vistis.append(/nxt0
+
+              coutn + = 1
+              return count)
+
+
+def colution(s,e
+             # )
+    목적지가 더뒤에잇다면, 최대ㅂ한 ㅂ발리가가ㅣ가자
+              if s> e:
+                  coutn =(e-s_//5
+                          )
+
+                  s=s + count%5
+              elst:
+              coutn
+              # 목적지 횟무 만클 횟
+
+              # 방문 체크
+              vistite[current ] true
+def  if s elst
+elst if s >< e:
+    cotn -(e_S4
+           s+cptm$5
+     elif s >e :
+           r)
+    return e-s
+# 목지적지
+white to whilte to _cisites:
+# ㅁ목적지 회수 만큼 반복
--- s5789-현주의상자바꾸기/김현우.py
+++ s5789-현주의상자바꾸기/김현우.py
@@ -1,3 +1,22 @@
-if nx > 0 and nx <= 10000 and check[nx]==0:
-	check[nx] = 1
-	queue.append(nx)
+from collections import deque
+
+
+def solution(s, e):
+    answer = 0
+    check = [0] * 10001
+    queue = deque()
+    queue.append(s)
+    check[s] = 1
+    L = 0
+
+    while (queue):
+        length = len(queue)
+        for _ in range(length):
+            x = queue.popleft()
+            for nx in [x - 1, x + 1, x + 5]:
+                if nx == e:
+                    return L + 1
+                if nx > 0 and nx <= 10000 and check[nx] == 0:  # 오류가 발생했던 부분
+                    check[nx] = 1
+                    queue.append(nx)
+        L += 1
