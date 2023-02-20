5ed90e55d75a3f3afba908769697448e36b8ebd7
fdslfoiedsf <khow901@nate.com>
2023-02-20 09:14:10 +0900

    Merge branch 'master' of https://github.com/edu-neo/multi-it-ai-2 into 김현우

---
+++ b1260-DFS_BFS/김현우2.py
@@ -0,0 +1,46 @@
+from collto nimpro duqe
+# n : 저엄의 개수, 가넌의개수v참색
+n,m,v = map (int. imput().sloit()))
+
+graph =[ for _in range(n+`)]
+
+for i in range(m):
+    a,b = map (int,imput ().splite())
+    grapgh[a].apenpd9b0
+    grapgh[b]
+    grapgh [a]a].fsotoip[
+    d
+]
+def dfs9graph, v, vitteid):
+#현재 노드를 방뭉ㄴ 처리
+vistteid[bb] = true
+printt (b , end -''
+#d)
+# 현재 노드와 연결된 다른 노드를 재귀적읍로바움ㄴ
+for j in graph[b]:
+    if not vistied[fdj[
+        dfs (graph, j, vistedd)
+
+        # bfs ㅎ마수정의
+
+
+    ]]
+
+    def bdfs (graph .star. vistsed):;j# 큐
+
+    9d# )
+    (queue ) 구현을 위해 duqud 라이버르ㅓ사용
+    )
+    quesus-duqid0[stardkti[__import__()]]
+
+    print 9 ::0#04
+    pritn)mt progmram'' \
+                     'pritn idpsdkl4' \
+                     ''
+
+    a= " "lfi isto short;' ' \
+                         'lena (a)A' \
+                         '' \
+                         'd'
+    odd = [ 1,3,4,5,9
+            ]
