1e1b939c715bc2097dcfc558f1019f6d8f802a27
minu-97 <alsdn9891@hanyang.ac.kr>
2023-02-20 17:43:23 +0900

    0220

--- s4837-부분집합의합/조민우.py
+++ s4837-부분집합의합/조민우.py
@@ -1,34 +1,45 @@
 import sys
-
 sys.stdin = open('input.txt')
-T = int(input())
+
 def dfs(idx):
-    global count, subset
-
+    global K, cnt, N
+    # 현재 idx 가 numbers의 길이와 같다 => 끝까지 갔다
     if idx == len(numbers):
-        return
-
-    if len(subset) == N:
+        # print(visited)
         if sum(subset) == K:
-            print(subset)
-            count += 1
+            if len(subset) == N:
+                cnt += 1
         return
 
     subset.append(numbers[idx])
-    dfs(idx+1)
-    subset.pop()
+    # visited[idx] = True
+
     dfs(idx+1)
 
+    subset.pop()
+    # visited[idx] = False
+
+    dfs(idx+1)
 
 T = int(input())
-
-for tc in range(1, T+ 1):
-    N, K = map(int, input().split())
-
-    numbers = [i for i in range(1, 13)]
+for test_case in range(1, T + 1):
+    N, K = map(int,input().split())
+    cnt = 0
+    # numbers = [1, 2, 3]
+    numbers = [_ for _ in range(1,13)]
+    # 부분집합 / 포함 여부 둘 중에 하나만 있어도 됨
+    # 부분 집합
     subset = []
+    # 포함 여부
+    # visited = [False] * 3
     count = 0
+
     dfs(0)
-    print(f'{tc} {count}')
+
+    print(f'#{test_case} {cnt}')
+
+
+
+
 
 
