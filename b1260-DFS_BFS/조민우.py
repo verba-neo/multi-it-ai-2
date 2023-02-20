397bd1409259242d5b8d83cefed37ab866773182
minu-97 <alsdn9891@hanyang.ac.kr>
2023-02-20 09:13:29 +0900

    solved

--- b1260-DFS_BFS/조민우.py
+++ b1260-DFS_BFS/조민우.py
@@ -7,23 +7,21 @@ graph = [[] for _ in range(N+1)]
 for _ in range(M):
     start, end = map(int, input().split())
     graph[start].append(end)
-    to_visits = [start]
-    # 양방향(무향)그래프 일 경우 아래 줄 추가
-    # graph[end].append(start)
-S,G = (start,end)
+    graph[end].append(start)
 
 def dfs():
     # S => G 로 갈 수 있는가?
     # DFS
     # 방문 여부
-    visited = [False for _ in range(M + 1)]
+    visited = [False for _ in range(N + 1)]
     # 목적지
-    to_visits = [S]
-
+    to_visits = [V]
     while to_visits:
         # 현재 위치 = 목적지들에서 마지막
         current = to_visits.pop()
         # 현재 위치를 방문한 적이 없다면,
+        graph[current].sort()
+        graph[current].reverse()
         if not visited[current]:
             print(current)
             # 현재 위치 방문 체크
@@ -31,28 +29,28 @@ def dfs():
             # 현재 위치에서 갈 수 있는 정점들을 목적지들에 추가
             to_visits += graph[current]
 
-
-print(dfs())
+            # to_visits.sort()
+dfs()
 
 
 def bfs():
     from collections import deque
 
     # 방문 여부
-    visited = [False for _ in range(M + 1)]
+    visited = [False for _ in range(N + 1)]
     # 목적지
     to_visits = deque()
-    to_visits.append(S)
+    to_visits.append(V)
 
     while to_visits:
         # 현재 위치 = 목적지들에서 마지막
         current = to_visits.popleft()
         # 현재 위치를 방문한 적이 없다면,
+        graph[current].sort()
         if not visited[current]:
             print(current)
             # 현재 위치 방문 체크
             visited[current] = True
             # 현재 위치에서 갈 수 있는 정점들을 목적지들에 추가
             to_visits += graph[current]
-
-print(bfs())
+bfs()
