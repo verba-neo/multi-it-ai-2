b90050e1da88e18a1559e27811a88f0ffd9c8e6d
minu-97 <alsdn9891@hanyang.ac.kr>
2023-02-21 13:57:33 +0900

    0221

---
+++ p121683-외톨이알파벳/조민우.py
@@ -0,0 +1,11 @@
+def solution(input_string):
+    answer = []
+    alpha = list(input_string)
+    check_alpha = []
+    check_num = 0
+
+    if alpha[0] != alpha[1] and alpha[0] == alpha[2]:
+        print(check_alpha.append(alpha.pop(0)))
+
+
+print(solution("edeaaabbccd"))
