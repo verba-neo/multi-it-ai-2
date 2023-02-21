1ddfb610a7439b270236deafe73feb09bf5fbced
leemoongi <leemoon91@naver.com>
2023-02-21 19:23:33 +0900

    leemoongi

---
+++ p121683-외톨이알파벳/이문기.py
@@ -0,0 +1,23 @@
+def solution(input_string):
+	length = len(input_string)
+	answer_stack = []
+	stack = []
+	for i in range(length):
+		if i == 0:
+			stack.append(input_string[i])
+		elif input_string[i] == input_string[i - 1]:
+			continue
+		elif input_string[i] not in stack:
+			stack.append(input_string[i])
+		elif input_string[i] in stack:
+			answer_stack.append(input_string[i])
+	answer = list(set([x for x in answer_stack]))
+	answer.sort()
+	if len(answer) == 0:
+		answer = 'N'
+	else:
+		answer = ''.join(str(x) for x in answer)
+	return answer
+
+x = 'zbzbz'
+print(solution(x))
