3e87bb924713574fb752f6d28a24ffc27943cbbf
jinred9 <jinred9@gmail.com>
2023-02-22 10:26:18 +0900

    s1209

--- s1209-sum/진홍구.py
+++ s1209-sum/진홍구.py
@@ -11,9 +11,13 @@ for _ in range(1, 11):
         row_matrix = list(map(int, input().split(' ')))
         sum_matrix.append(row_matrix)
     for i in range(100):
+        # column 합구하기 초기화
         sum_column_list = 0
+        # cross 합구하기
         sum_cross_list += sum_matrix[i][i]
+        # 역 cross 합구하기
         sum_cross_div_list += sum_matrix[99-i][i]
+        # column 합구하기
         for j in range(100):
             sum_column_list += sum_matrix[j][i]
         if sum_max < max(sum(sum_matrix[i]), sum_column_list):
