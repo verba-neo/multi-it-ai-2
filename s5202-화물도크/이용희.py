c5b00257b807c13d9fca468b185cd29e15b22d06
leeyonghee1017 <leeyonghe90@gmail.com>
2023-02-23 16:55:26 +0900

    s5201

--- s5202-화물도크/이용희.py
+++ s5202-화물도크/이용희.py
@@ -7,7 +7,6 @@ for test_case in range(1, T + 1):
     N = int(input())
     schedules = [list(map(int, input().split())) for _ in range(N)]
     schedules.sort(key=lambda x: (x[1], x[0]))
-
     answer = 0
     last = 0
     for s, e in schedules:
